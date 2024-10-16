7afa9f18ee59c6752c16b9142e6503e8804da603
EC2 Default User <ec2-user@ip-172-31-45-227.ec2.internal>
2024-10-16 01:29:57 +0000

    destroy

--- network.tf
+++ network.tf
@@ -93,3 +93,44 @@ resource "aws_route_table_association" "private_rt_associate" {
   subnet_id      = aws_subnet.private_subnet.id
   route_table_id = aws_route_table.private_rt.id
 }
+module "app_sg" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = "App-SG"
+  description = "App SG for web app"
+  vpc_id      = data.aws_vpc.minha_vpc.id
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      description = "Porta para minha aplicacao web de acesso"
+      cidr_blocks = "0.0.0.0/0"
+    }
+  ]
+  egress_rules        = ["all-all"]
+}
+
+module "ec2_app_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 3.0"
+
+  name = "Web-App-Instance"
+
+  ami                    = "ami-07caf09b362be10b8"
+  instance_type          = "t2.micro"
+  key_name               = "vockey"
+  monitoring             = true
+  vpc_security_group_ids = [module.app_sg.security_group_id]
+  subnet_id              = var.subnet_id
+  user_data              = file("./dependencias.sh")
+}
+
+resource "aws_eip" "ec2_app_instance_ip" {
+  instance = module.ec2_app_instance.id
+  domain = "vpc"
+
+  tags = {
+    Name = "Web-Server-EIP"
+  }
+}
